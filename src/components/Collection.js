53a7ed774a88c3f228bbadc0e54b675ec1a857ac
jayantwaldia <imjayantwaldia@gmail.com>
2022-08-18 15:34:40 +0530

    visual changes

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -1,14 +1,28 @@
-import React from "react";
+import item1 from "../assets/item-1.jpg";
+import item2 from "../assets/item-2.webp";
+import item3 from "../assets/item-3.webp";
+import item4 from "../assets/item-4.webp";
 
 function Collection() {
   return (
     <>
       <div className="collection">
-        <p style={{ fontFamily: "Optivenus" }}>THE COLLECTION</p>
-        <div className="shop-grid">
-          <div className="item-1"></div>
-          <div className="item-1"></div>
-          <div className="item-1"></div>
+        <h1 style={{ fontFamily: "Optivenus" }}>THE COLLECTION</h1>
+        <div className="items">
+          <div className="shop-grid">
+            <div className="item-1">
+              <img src={item1} alt="product"></img>
+            </div>
+            <div className="item-1">
+              <img src={item2} alt="product"></img>
+            </div>
+            <div className="item-1">
+              <img src={item3} alt="product"></img>
+            </div>
+            <div className="item-1">
+              <img src={item4} alt="product"></img>
+            </div>
+          </div>
         </div>
       </div>
     </>
