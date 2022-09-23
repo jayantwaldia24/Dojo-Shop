d43ddfd012456e33c34290e870fbf3544466f0b0
jayantwaldia <imjayantwaldia@gmail.com>
2022-09-23 13:38:24 +0530

    mapping over productsList

---
+++ src/backend/db/products.js
@@ -0,0 +1,43 @@
+import item1 from "../../toolkit/assets/item-1.png";
+import item2 from "../../toolkit/assets/item-2.png";
+import item3 from "../../toolkit/assets/item-3.png";
+import item4 from "../../toolkit/assets/item-4.png";
+
+export const products = [
+  {
+    id: 1,
+    brand: "Nike Air",
+    name: "Women's Running Jacket",
+    price: 5995,
+    img: item1,
+    category: "women",
+    inStock: true,
+  },
+  {
+    id: 2,
+    brand: "Jordan Essentials",
+    name: "Men's Graphic T-Shirt",
+    price: 1795,
+    img: item2,
+    category: "men",
+    inStock: true,
+  },
+  {
+    id: 3,
+    brand: "Nike Yoga Dri-FIT Luxe",
+    name: "Women's Cropped Tank",
+    price: 2895,
+    img: item3,
+    category: "women",
+    inStock: false,
+  },
+  {
+    id: 4,
+    brand: "Nike Dri-FIT Legend",
+    name: "Men's Graphic Training T-Shirt",
+    price: 1795,
+    img: item4,
+    category: "men",
+    inStock: true,
+  },
+];
--- src/components/Collection.js
+++ src/components/Collection.js
@@ -1,7 +1,4 @@
-import item1 from "../toolkit/assets/item-1.png";
-import item2 from "../toolkit/assets/item-2.png";
-import item3 from "../toolkit/assets/item-3.png";
-import item4 from "../toolkit/assets/item-4.png";
+import { products } from "../backend/db/products";
 
 function Collection() {
   return (
@@ -9,40 +6,25 @@ function Collection() {
       <h1 className="max-w-[1240px] m-[80px] font-bold text-center mx-auto text-3xl md:text-5xl font-optivenus ">
         THE COLLECTION
       </h1>
-      <div className="max-w-[1240px] mx-auto mt-[50px] grid md:grid-cols-3 sm:grid-cols-2 border-t-[1px] border-l-[1px] border-black">
-        <div className="border-r-[1px] border-b-[1px] border-black">
-          <div className="p-5 pb-0">
-            <p className="text-xl">Nike Air</p>
-            <p>Women's Running Jacket</p>
-            <p className="font-bold mt-2">₹ 5,995</p>
-          </div>
 
-          <img className="w-[80%] mx-auto" src={item1} alt="item1"></img>
-        </div>
-        <div className="border-r-[1px] border-b-[1px] border-black">
-          <div className="p-5 pb-0">
-            <p className="text-xl">Jordan Essentials</p>
-            <p>Men's Graphic T-Shirt</p>
-            <p className="font-bold mt-2">₹ 1,795</p>
+      <div className="max-w-[1240px] mx-auto mt-[50px] grid md:grid-cols-3 sm:grid-cols-2 border-t-[1px] border-l-[1px] border-black">
+        {products.map((product) => (
+          <div className="border-r-[1px] border-b-[1px] border-black">
+            <div className="p-5 pb-0">
+              <p className="text-xl">{product.brand}</p>
+              <p>{product.name}</p>
+              <p className="font-bold mt-2">
+                ₹{" "}
+                {product.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+              </p>
+            </div>
+            <img
+              className="w-[80%] mx-auto"
+              src={product.img}
+              alt={product.img}
+            ></img>
           </div>
-          <img className="w-[80%] mx-auto" src={item2} alt="item-2"></img>
-        </div>
-        <div className="border-r-[1px] border-b-[1px] border-black">
-          <div className="p-5 pb-0">
-            <p className="text-xl">Nike Yoga Dri-FIT Luxe</p>
-            <p>Women's Cropped Tank</p>
-            <p className="font-bold mt-2">₹ 2,895</p>
-          </div>
-          <img className="w-[80%] mx-auto" src={item3} alt="item-3"></img>
-        </div>
-        <div className="border-r-[1px] border-b-[1px] border-black">
-          <div className="p-5 pb-0">
-            <p className="text-xl">Nike Dri-FIT Legend</p>
-            <p>Men's Graphic Training T-Shirt</p>
-            <p className="font-bold mt-2">₹ 1,795</p>
-          </div>
-          <img className="w-[80%] mx-auto" src={item4} alt="item-4"></img>
-        </div>
+        ))}
       </div>
     </div>
   );
