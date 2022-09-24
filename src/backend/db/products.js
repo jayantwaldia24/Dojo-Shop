08c4255ea14f63b11a2956a757024bc4b9ab1389
jayantwaldia <imjayantwaldia@gmail.com>
2022-09-24 16:33:13 +0530

    individual product page completed

--- src/backend/db/products.js
+++ src/backend/db/products.js
@@ -1,7 +1,23 @@
 import item1 from "../../toolkit/assets/item-1.png";
+import item1_1 from "../../toolkit/assets/item-1-1.jpg";
+import item1_2 from "../../toolkit/assets/item-1-2.webp";
+import item1_3 from "../../toolkit/assets/item-1-3.png";
+import item1_4 from "../../toolkit/assets/item-1-4.jpg";
 import item2 from "../../toolkit/assets/item-2.png";
+import item2_1 from "../../toolkit/assets/item-2-1.webp";
+import item2_2 from "../../toolkit/assets/item-2-2.webp";
+import item2_3 from "../../toolkit/assets/item-2-3.webp";
+import item2_4 from "../../toolkit/assets/item-2-4.webp";
 import item3 from "../../toolkit/assets/item-3.png";
+import item3_1 from "../../toolkit/assets/item-3-1.webp";
+import item3_2 from "../../toolkit/assets/item-3-2.jpg";
+import item3_3 from "../../toolkit/assets/item-3-3.jpg";
+import item3_4 from "../../toolkit/assets/item-3-4.jpg";
 import item4 from "../../toolkit/assets/item-4.png";
+import item4_1 from "../../toolkit/assets/item-4-1.jpg";
+import item4_2 from "../../toolkit/assets/item-4-2.jpg";
+import item4_3 from "../../toolkit/assets/item-4-3.jpg";
+import item4_4 from "../../toolkit/assets/item-4-4.jpg";
 
 export const products = [
   {
@@ -11,47 +27,38 @@ export const products = [
     price: 5995,
     img: item1,
     category: "women",
-    inStock: true,
     href: "#",
     breadcrumbs: [
-      { id: 1, name: "Men", href: "#" },
-      { id: 2, name: "Clothing", href: "#" },
+      { id: 1, name: "Women", href: "#" },
+      { id: 2, name: "Collection", href: "#" },
     ],
     images: [
       {
-        src: "https://tailwindui.com/img/ecommerce-images/product-page-02-secondary-product-shot.jpg",
+        src: item1_4,
         alt: "Two each of gray, white, and black shirts laying flat.",
       },
       {
-        src: "https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-01.jpg",
+        src: item1_2,
         alt: "Model wearing plain black basic tee.",
       },
       {
-        src: "https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-02.jpg",
+        src: item1_3,
         alt: "Model wearing plain gray basic tee.",
       },
       {
-        src: "https://tailwindui.com/img/ecommerce-images/product-page-02-featured-product-shot.jpg",
+        src: item1_1,
         alt: "Model wearing plain white basic tee.",
       },
     ],
-    colors: [
-      { name: "White", class: "bg-white", selectedClass: "ring-gray-400" },
-      { name: "Gray", class: "bg-gray-200", selectedClass: "ring-gray-400" },
-      { name: "Black", class: "bg-gray-900", selectedClass: "ring-gray-900" },
-    ],
     sizes: [
-      { name: "XXS", inStock: false },
-      { name: "XS", inStock: true },
+      { name: "XS", inStock: false },
       { name: "S", inStock: true },
       { name: "M", inStock: true },
       { name: "L", inStock: true },
       { name: "XL", inStock: true },
-      { name: "2XL", inStock: true },
-      { name: "3XL", inStock: true },
     ],
     description:
-      'The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our white tee has you covered.',
+      "The Nike Air Jacket is a celebration of your outdoor journey, of the time spent in solitude as you chase your running goals. Breathable at the back, with coverage up top, the jacket is geared to keep you feeling loose with every step. A bungee at the back lets you control the shape and feel along your route.",
     highlights: [
       "Hand cut and sewn locally",
       "Dyed with our proprietary colors",
@@ -68,7 +75,46 @@ export const products = [
     price: 1795,
     img: item2,
     category: "men",
-    inStock: true,
+    href: "#",
+    breadcrumbs: [
+      { id: 1, name: "Men", href: "#" },
+      { id: 2, name: "Collection", href: "#" },
+    ],
+    images: [
+      {
+        src: item2_4,
+        alt: "Two each of gray, white, and black shirts laying flat.",
+      },
+      {
+        src: item2_2,
+        alt: "Model wearing plain black basic tee.",
+      },
+      {
+        src: item2_3,
+        alt: "Model wearing plain gray basic tee.",
+      },
+      {
+        src: item2_1,
+        alt: "Model wearing plain white basic tee.",
+      },
+    ],
+    sizes: [
+      { name: "XS", inStock: false },
+      { name: "S", inStock: true },
+      { name: "M", inStock: true },
+      { name: "L", inStock: true },
+      { name: "XL", inStock: true },
+    ],
+    description:
+      "Take flight in signature style. Made from soft, comfortable cotton, this relaxed tee has printed graphics on the chest.",
+    highlights: [
+      "Hand cut and sewn locally",
+      "Dyed with our proprietary colors",
+      "Pre-washed & pre-shrunk",
+      "Ultra-soft 100% cotton",
+    ],
+    details:
+      'The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming "Charcoal Gray" limited release.',
   },
   {
     _id: 3,
@@ -77,7 +123,46 @@ export const products = [
     price: 2895,
     img: item3,
     category: "women",
-    inStock: false,
+    href: "#",
+    breadcrumbs: [
+      { id: 1, name: "Women", href: "#" },
+      { id: 2, name: "Collection", href: "#" },
+    ],
+    images: [
+      {
+        src: item3_4,
+        alt: "Two each of gray, white, and black shirts laying flat.",
+      },
+      {
+        src: item3_2,
+        alt: "Model wearing plain black basic tee.",
+      },
+      {
+        src: item3_3,
+        alt: "Model wearing plain gray basic tee.",
+      },
+      {
+        src: item3_1,
+        alt: "Model wearing plain white basic tee.",
+      },
+    ],
+    sizes: [
+      { name: "XS", inStock: false },
+      { name: "S", inStock: true },
+      { name: "M", inStock: true },
+      { name: "L", inStock: true },
+      { name: "XL", inStock: true },
+    ],
+    description:
+      "The Nike Yoga Dri-FIT Luxe Cropped Tank is made from Infinalon fabric that feels irresistibly soft and smooth while providing a gently compressive feel that hugs your body, so you can stretch and move without limits. Mixed-material panels create a fluid look and extra breathability while you move.",
+    highlights: [
+      "Hand cut and sewn locally",
+      "Dyed with our proprietary colors",
+      "Pre-washed & pre-shrunk",
+      "Ultra-soft 100% cotton",
+    ],
+    details:
+      'The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming "Charcoal Gray" limited release.',
   },
   {
     _id: 4,
@@ -86,6 +171,45 @@ export const products = [
     price: 1795,
     img: item4,
     category: "men",
-    inStock: true,
+    href: "#",
+    breadcrumbs: [
+      { id: 1, name: "Men", href: "#" },
+      { id: 2, name: "Collection", href: "#" },
+    ],
+    images: [
+      {
+        src: item4_4,
+        alt: "Two each of gray, white, and black shirts laying flat.",
+      },
+      {
+        src: item4_2,
+        alt: "Model wearing plain black basic tee.",
+      },
+      {
+        src: item4_3,
+        alt: "Model wearing plain gray basic tee.",
+      },
+      {
+        src: item4_1,
+        alt: "Model wearing plain white basic tee.",
+      },
+    ],
+    sizes: [
+      { name: "XS", inStock: false },
+      { name: "S", inStock: true },
+      { name: "M", inStock: true },
+      { name: "L", inStock: true },
+      { name: "XL", inStock: true },
+    ],
+    description:
+      "Commit to the grind and flex hard in the Nike Legend T-Shirt. Made with soft, lightweight jersey fabric, it feels silky smooth on your skin and wicks sweat to keep you dry.",
+    highlights: [
+      "Hand cut and sewn locally",
+      "Dyed with our proprietary colors",
+      "Pre-washed & pre-shrunk",
+      "Ultra-soft 100% cotton",
+    ],
+    details:
+      'The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming "Charcoal Gray" limited release.',
   },
 ];
--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -6,65 +6,14 @@ import { RadioGroup } from "@headlessui/react";
 
 function ProductPage() {
   const { id } = useParams();
-  const product = {
-    name: "Basic Tee 6-Pack",
-    price: "$192",
-    href: "#",
-    breadcrumbs: [
-      { id: 1, name: "Men", href: "#" },
-      { id: 2, name: "Clothing", href: "#" },
-    ],
-    images: [
-      {
-        src: "https://tailwindui.com/img/ecommerce-images/product-page-02-secondary-product-shot.jpg",
-        alt: "Two each of gray, white, and black shirts laying flat.",
-      },
-      {
-        src: "https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-01.jpg",
-        alt: "Model wearing plain black basic tee.",
-      },
-      {
-        src: "https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-02.jpg",
-        alt: "Model wearing plain gray basic tee.",
-      },
-      {
-        src: "https://tailwindui.com/img/ecommerce-images/product-page-02-featured-product-shot.jpg",
-        alt: "Model wearing plain white basic tee.",
-      },
-    ],
-    colors: [
-      { name: "White", class: "bg-white", selectedClass: "ring-gray-400" },
-      { name: "Gray", class: "bg-gray-200", selectedClass: "ring-gray-400" },
-      { name: "Black", class: "bg-gray-900", selectedClass: "ring-gray-900" },
-    ],
-    sizes: [
-      { name: "XXS", inStock: false },
-      { name: "XS", inStock: true },
-      { name: "S", inStock: true },
-      { name: "M", inStock: true },
-      { name: "L", inStock: true },
-      { name: "XL", inStock: true },
-      { name: "2XL", inStock: true },
-      { name: "3XL", inStock: true },
-    ],
-    description:
-      'The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our white tee has you covered.',
-    highlights: [
-      "Hand cut and sewn locally",
-      "Dyed with our proprietary colors",
-      "Pre-washed & pre-shrunk",
-      "Ultra-soft 100% cotton",
-    ],
-    details:
-      'The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming "Charcoal Gray" limited release.',
-  };
-  console.log(product, id);
+  const product = products.find((p) => p._id == id);
   const reviews = { href: "#", average: 4, totalCount: 117 };
+  console.log(product);
 
   function classNames(...classes) {
     return classes.filter(Boolean).join(" ");
   }
-  const [selectedColor, setSelectedColor] = useState(product.colors[0]);
+
   const [selectedSize, setSelectedSize] = useState(product.sizes[2]);
   return (
     <>
@@ -148,15 +97,23 @@ function ProductPage() {
           <div className="mx-auto max-w-2xl px-4 pt-10 pb-16 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pt-16 lg:pb-24">
             <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
               <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
-                {product.name}
+                {product.brand}
               </h1>
+              <div className="mt-10">
+                <h2 className="text-sm font-medium text-gray-900">Details</h2>
+
+                <div className="mt-4 space-y-6">
+                  <p className="text-sm text-gray-600">{product.name}</p>
+                </div>
+              </div>
             </div>
 
             {/* Options */}
             <div className="mt-4 lg:row-span-3 lg:mt-0">
               <h2 className="sr-only">Product information</h2>
               <p className="text-3xl tracking-tight text-gray-900">
-                {product.price}
+                ₹{" "}
+                {product.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
               </p>
 
               {/* Reviews */}
@@ -188,50 +145,6 @@ function ProductPage() {
               </div>
 
               <form className="mt-10">
-                {/* Colors */}
-                <div>
-                  <h3 className="text-sm font-medium text-gray-900">Color</h3>
-
-                  <RadioGroup
-                    value={selectedColor}
-                    onChange={setSelectedColor}
-                    className="mt-4"
-                  >
-                    <RadioGroup.Label className="sr-only">
-                      {" "}
-                      Choose a color{" "}
-                    </RadioGroup.Label>
-                    <div className="flex items-center space-x-3">
-                      {product.colors.map((color) => (
-                        <RadioGroup.Option
-                          key={color.name}
-                          value={color}
-                          className={({ active, checked }) =>
-                            classNames(
-                              color.selectedClass,
-                              active && checked ? "ring ring-offset-1" : "",
-                              !active && checked ? "ring-2" : "",
-                              "-m-0.5 relative p-0.5 rounded-full flex items-center justify-center cursor-pointer focus:outline-none"
-                            )
-                          }
-                        >
-                          <RadioGroup.Label as="span" className="sr-only">
-                            {" "}
-                            {color.name}{" "}
-                          </RadioGroup.Label>
-                          <span
-                            aria-hidden="true"
-                            className={classNames(
-                              color.class,
-                              "h-8 w-8 border border-black border-opacity-10 rounded-full"
-                            )}
-                          />
-                        </RadioGroup.Option>
-                      ))}
-                    </div>
-                  </RadioGroup>
-                </div>
-
                 {/* Sizes */}
                 <div className="mt-10">
                   <div className="flex items-center justify-between">
@@ -348,14 +261,6 @@ function ProductPage() {
                       </li>
                     ))}
                   </ul>
-                </div>
-              </div>
-
-              <div className="mt-10">
-                <h2 className="text-sm font-medium text-gray-900">Details</h2>
-
-                <div className="mt-4 space-y-6">
-                  <p className="text-sm text-gray-600">{product.name}</p>
                 </div>
               </div>
             </div>
